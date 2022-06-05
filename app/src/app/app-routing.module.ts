46387a3d577acde96d7df5521cd53c7b5ba3d6aa
Lyly-Carolayne <69611981+Lyly-Carolayne@users.noreply.github.com>
2022-06-04 21:23:46 -0700

    Add files via upload

---
+++ app/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import{  HomePageComponent } from'./components/home-page/home-page.component';
+import{ LandingPageComponent } from './components/landing-page/landing-page.component';
+
+const routes: Routes = [
+  {path:'', component: HomePageComponent },
+  {path:'landing', component: LandingPageComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ app/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { SubscribeComponent } from './components/subscribe/subscribe.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomePageComponent,
+    LandingPageComponent,
+    SubscribeComponent,
+    ProductPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
